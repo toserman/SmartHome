1c6f86fe8066b62131b4153a9241bb6023ebd74a
toserman <nesterae1980@gmail.com>
2017-12-29 00:04:21 +0200

    Added buttons

--- app/src/main/java/com/example/antonio/smarthome/MainActivity.java
+++ app/src/main/java/com/example/antonio/smarthome/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.antonio.smarthome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView tview_log;
+    Button btn_start, btn_stop;
+    private static final String TAG = MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = (Button)findViewById(R.id.srv_start);
+        btn_stop = (Button)findViewById(R.id.srv_stop);
+        tview_log = (TextView)findViewById(R.id.output);
+
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+
     }
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.srv_start:
+                Log.d(TAG,"Button: SERVER START");
+                break;
+            case R.id.srv_stop:
+                Log.d(TAG,"Button: SERVER STOP");
+                break;
+        }
+
+    }
+
+
 }
